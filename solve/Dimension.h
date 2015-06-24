f6ac16dd16b22a990974562d631257bfcef5d45b
Tom Ward <tom.c.ward@gmail.com>
2015-06-24 09:37:24 +0100

    Introduce Unit class, replacing Dimension for constraint checking only

--- solve/Dimension.h
+++ solve/Dimension.h
@@ -1,31 +1,5 @@
 #include <iostream>
 
-namespace hummingbird {
-
-template <typename T, int m, int l, int t>
-class Dimension
-{
-public:
-	explicit Dimension(const T& val) : value_(val) {}
-
-	T& value() { return value_; }
-	const T& value() const { return value_; }
-
-	Dimension& operator+=(const Dimension& rhs) { value_ += rhs; return *this; }
-	Dimension& operator-=(const Dimension& rhs) { value_ -= rhs; return *this; }
-	template <typename S>
-	Dimension& operator*=(const S& rhs) { value_ *= rhs; return *this; }
-	template <typename S>
-	Dimension& operator/=(const S& rhs) { value_ /= rhs; return *this; }
-
-private:
-	T value_;
-
-	friend std::ostream& operator<<(std::ostream& os, const Dimension& dimension) {
-		return os << dimension.value_ << " units";
-	}
-};
-
 namespace
 {
 	template <typename X, typename Y>
@@ -33,58 +7,100 @@ namespace
 
 	template <typename X, typename Y>
 	using MulType = decltype(std::declval<X>() * std::declval<Y>());
+
+	template <typename D1, typename D2>
+	using DivType = decltype(std::declval<D1>() / std::declval<D2>());
 }
 
-template <typename X, typename Y, int m, int l, int t>
-Dimension<AddType<X,Y>, m, l, t> operator+(const Dimension<X, m, l, t>& lhs,
-	                                       const Dimension<Y, m, l, t>& rhs) {
-	return Dimension<AddType<X,Y>, m, l, t>(lhs.value() + rhs.value());
+
+namespace hummingbird {
+
+template <int m, int l, int t>
+class Dimension {};
+
+template <int m1, int l1, int t1, int m2, int l2, int t2>
+Dimension<m1+m2, l1+l2, t1+t2> operator*(Dimension<m1, l1, t1> lhs,
+	                                     Dimension<m2, l2, t2> rhs) {
+	return Dimension<m1+m2, l1+l2, t1+t2>();
 }
 
-template <typename X, typename Y, int m, int l, int t>
-Dimension<AddType<X,Y>, m, l, t> operator-(const Dimension<X, m, l, t>& lhs,
-	                                       const Dimension<Y, m, l, t>& rhs) {
-	return Dimension<AddType<X,Y>, m, l, t>(lhs.value() - rhs.value());
+template <int m1, int l1, int t1, int m2, int l2, int t2>
+Dimension<m1-m2, l1-l2, t1-t2> operator/(Dimension<m1, l1, t1> lhs,
+	                                     Dimension<m2, l2, t2> rhs) {
+	return Dimension<m1-m2, l1-l2, t1-t2>();
 }
 
-template <typename T, int m1, int l1, int t1, typename S>
-Dimension<T, m1, l1, t1> operator*(Dimension<T, m1, l1, t1> copy, const S& rhs) {
+template <typename T, typename D>
+class Unit
+{
+public:
+	explicit Unit(const T& val) : value_(val) {}
+
+	T& value() { return value_; }
+	const T& value() const { return value_; }
+
+	Unit& operator+=(const Unit& rhs) { value_ += rhs; return *this; }
+	Unit& operator-=(const Unit& rhs) { value_ -= rhs; return *this; }
+	template <typename S>
+	Unit& operator*=(const S& rhs) { value_ *= rhs; return *this; }
+	template <typename S>
+	Unit& operator/=(const S& rhs) { value_ /= rhs; return *this; }
+
+private:
+	T value_;
+
+	friend std::ostream& operator<<(std::ostream& os, const Unit& quantity) {
+		return os << quantity.value_ << " units";
+	}
+};
+
+
+
+template <typename X, typename Y, typename D>
+Unit<AddType<X,Y>, D> operator+(const Unit<X,D>& lhs,
+	                            const Unit<Y,D>& rhs) {
+	return Unit<AddType<X,Y>, D>(lhs.value() + rhs.value());
+}
+
+template <typename X, typename Y, typename D>
+Unit<AddType<X,Y>, D> operator-(const Unit<X,D>& lhs,
+	                            const Unit<Y,D>& rhs) {
+	return Unit<AddType<X,Y>, D>(lhs.value() - rhs.value());
+}
+
+template <typename T, typename D, typename S>
+Unit<T,D> operator*(Unit<T,D> copy, const S& rhs) {
 	return copy *= rhs;
 }
 
-template <typename T, int m1, int l1, int t1, typename S>
-Dimension<T, m1, l1, t1> operator*(const S& lhs, Dimension<T, m1, l1, t1> copy) {
+template <typename T, typename D, typename S>
+Unit<T,D> operator*(const S& lhs, Unit<T, D> copy) {
 	return copy *= lhs;
 }
 
-template <typename X, typename Y, int m1, int l1, int t1,
-                      			  int m2, int l2, int t2>
-Dimension<MulType<X,Y>, m1+m2, l1+l2, t1+t2> operator*(const Dimension<X, m1, l1, t1>& lhs,
-	                                                   const Dimension<Y, m2, l2, t2>& rhs) {
-	return Dimension<MulType<X,Y>, m1+m2, l1+l2, t1+t2>(lhs.value() * rhs.value());
+template <typename X, typename Y, typename D1, typename D2>
+Unit<MulType<X,Y>, MulType<D1,D2>> operator*(const Unit<X,D1>& lhs,
+	                                        const Unit<Y,D2>& rhs) {
+	return Unit<MulType<X,Y>, MulType<D1,D2>>(lhs.value() * rhs.value());
 }
 
-template <typename X, typename Y, int m1, int l1, int t1,
-                      			  int m2, int l2, int t2>
-Dimension<MulType<X,Y>, m1-m2, l1-l2, t1-t2> operator/(const Dimension<X, m1, l1, t1>& lhs,
-	                                                   const Dimension<Y, m2, l2, t2>& rhs) {
-	return Dimension<MulType<X,Y>, m1-m2, l1-l2, t1-t2>(lhs.value() / rhs.value());
+template <typename X, typename Y, typename D1, typename D2>
+Unit<MulType<X,Y>, DivType<D1,D2>> operator/(const Unit<X,D1>& lhs,
+	                                        const Unit<Y,D2>& rhs) {
+	return Unit<MulType<X,Y>, DivType<D1,D2>>(lhs.value() / rhs.value());
 }
 
+using Mass = Dimension<1,0,0>;
+using Length = Dimension<0,1,0>;
+using Time = Dimension<0,0,1>;
+using Velocity = Dimension<0,1,-1>;
 
-using Mass = Dimension<float,1,0,0>;
-using Length = Dimension<float,0,1,0>;
-using Time = Dimension<float,0,0,1>;
+Unit<float, Mass> kilogram(1.0);
 
+template <typename T>
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const Unit<T, Velocity>& mps)
+{ return os << mps.value() << " m/s"; }
 
-template <typename D>
-class Quantity
-{
-
-};
-
-Mass kilogram(1.0);
-//Quantity<Mass> kilogram(1.0);
 
 }
-
--- tests/DimensionTest.cpp
+++ tests/DimensionTest.cpp
@@ -7,12 +7,23 @@ using namespace hummingbird;
 
 TEST(UnitsTest, Start)
 {
-	Dimension<float,1,0,0> two(2.f);
-	cout << "1: " << two * two * 1.25f << endl;
+	Unit<float, Mass> m1(1.5);
+	Unit<float, Mass> m2(1.4);
+	Unit<float, Length> l1(1.1);
+	Unit<float, Time> t1(1.9);
+	auto vel = l1 / t1;
+	Unit<float, DivType<Length,Time>> vel2 = l1 / t1;
+	auto vel3 = l1 / t1;
 
-	Length l1(2);
-	Length l2(4.5f);
-	Mass m1(4.5f);
-	cout << "2: " << l1 + l2 << endl;
-	//cout << "2: " << l1 + l2 + m1 << endl;
+	cout << "vel: " << vel + vel3 << endl;
+
+	Dimension<1,0,0> d1;
+	Dimension<0,1,0> d2;
+	d1 * d2;
+
+	MulType<Dimension<1,0,0>, Dimension<0,1,0>> d3;
+	MulType<Mass, Length> ML;
+
+	Unit<float, MulType<Dimension<1,0,0>, Dimension<0,1,0>>> u1(2);
+	Unit<float, MulType<Mass, Length>> u2(2);
 }
