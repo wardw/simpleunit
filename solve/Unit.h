f4043e0a103b1a1e664e0f97775692c29c4df651
Tom Ward <tom.c.ward@gmail.com>
2015-06-25 13:00:40 +0100

    In progress: Start of Quantity type based on std::ratio<> (and similar in effect to chrono::duration)

--- solve/Unit.h
+++ solve/Unit.h
@@ -1,7 +1,17 @@
 #include <iostream>
 
+
+// NFUL. No frills units library inspired on Barton & Nackman's Dimensional Analysis (Ch 16.5)
+// and combined with std::ratio
+
+// Library consists of a minimal subset of features ..
+
 namespace
 {
+	// Todo: I could use std::common_type
+	// + which is already defined for built-ins (X,Y)
+	// + and overloading for Dimension would be to replace the existing Dimension operators (?)
+
 	template <typename X, typename Y>
 	using AddType = decltype(std::declval<X>() + std::declval<Y>());
 
@@ -78,15 +88,20 @@ Unit<T,D> operator*(const S& lhs, Unit<T, D> copy) {
 	return copy *= lhs;
 }
 
+template <typename T, typename D, typename S>
+Unit<T,D> operator/(Unit<T,D> copy, const S& rhs) {
+	return copy /= rhs;
+}
+
 template <typename X, typename Y, typename D1, typename D2>
 Unit<MulType<X,Y>, MulType<D1,D2>> operator*(const Unit<X,D1>& lhs,
-	                                        const Unit<Y,D2>& rhs) {
+	                                         const Unit<Y,D2>& rhs) {
 	return Unit<MulType<X,Y>, MulType<D1,D2>>(lhs.value() * rhs.value());
 }
 
 template <typename X, typename Y, typename D1, typename D2>
 Unit<MulType<X,Y>, DivType<D1,D2>> operator/(const Unit<X,D1>& lhs,
-	                                        const Unit<Y,D2>& rhs) {
+	                                         const Unit<Y,D2>& rhs) {
 	return Unit<MulType<X,Y>, DivType<D1,D2>>(lhs.value() / rhs.value());
 }
 
@@ -96,14 +111,86 @@ using Time = Dimension<0,0,1>;
 using Velocity = Dimension<0,1,-1>;
 
 Unit<float, Mass> kilogram(1.0);
+Unit<float, Mass> gram(0.001);
 
 template <typename T>
-std::ostream& operator<<(std::ostream& os, const Unit<T, Mass>& mps)
-{ return os << mps.value() << " m/s"; }
+std::ostream& operator<<(std::ostream& os, const Unit<T, Mass>& unit)
+{ return os << unit.value() << " kg"; }
 
 template <typename T>
-std::ostream& operator<<(std::ostream& os, const Unit<T, Velocity>& mps)
-{ return os << mps.value() << " m/s"; }
+std::ostream& operator<<(std::ostream& os, const Unit<T, Velocity>& unit)
+{ return os << unit.value() << " m/s"; }
+
+template <typename R1, typename R2>
+using IsMultiple = std::integral_constant<bool, (R1::num*R2::den) % (R2::num*R1::den) == 0>;
+
+template <typename T, typename Ratio = std::ratio<1>>
+class Quantity
+{
+public:
+	using rep = T;
+	using unit = Ratio;
+
+	Quantity(const T& val) : value_(val) {}
+
+	template < typename X, typename R1,
+		typename = std::enable_if_t<
+			IsMultiple<R1,Ratio>::value &&
+		    std::is_integral<T>::value &&
+		    std::is_integral<X>::value> >
+	Quantity(const Quantity<X,R1>& rhs) {
+		// New value is target quantity / this ratio, which enable_if guarantees to divide evenly
+		value_ = rhs.value() * R1::num * Ratio::den / (R1::den * Ratio::num);
+	}
+
+	// template < typename X, typename R1,
+	// 	typename = std::enable_if_t<
+	// 		//IsMultiple<R1,Ratio>::value &&
+	// 	    std::is_floating_point<T>::value &&
+	// 	    std::is_floating_point<X>::value> >
+	// Quantity(const Quantity<X,R1>& rhs) {
+	// 	// New value is target quantity / this ratio, which enable_if guarantees to divide evenly
+	// 	value_ = rhs.value() * R1::num * Ratio::den / (R1::den * Ratio::num);
+	// }
+
+	T& value() { return value_; }
+	const T& value() const { return value_; }
+
+	Quantity& operator+=(const Quantity& rhs) { value_ += rhs.value(); return *this; }
+
+	friend std::ostream& operator<<(std::ostream& os, const Quantity& q) {
+		return os << q.value() << " (" << Ratio::num << "/" << Ratio::den << ")";
+	}
+
+private:
+	T value_;
+};
+
+template <typename R1, typename R2>
+using BaseUnit = std::ratio<1, R1::den*R2::den>;
+
+template <typename X, typename Y, typename R1, typename R2>
+Quantity<AddType<X,Y>, BaseUnit<R1,R2>> operator+(const Quantity<X,R1>& lhs, const Quantity<Y,R2>& rhs)
+{
+	return Quantity<AddType<X,Y>, BaseUnit<R1,R2>>(R1::num * R2::den * lhs.value() +
+												   R2::num * R1::den * rhs.value());
+}
+
+template <typename X, typename Y, typename R1, typename R2>
+Quantity<MulType<X,Y>, std::ratio_multiply<R1,R2>> operator*(const Quantity<X,R1>& lhs, const Quantity<Y,R2>& rhs)
+{
+	return Quantity<MulType<X,Y>, std::ratio_multiply<R1,R2>>(lhs.value() * rhs.value());
+}
+
+
+// Helper types
+
+using Meter = Quantity<float, std::ratio<1,1>>;
+using Centimeter = Quantity<int, std::ratio<1,100>>;
+using Millimeter = Quantity<int, std::milli>;
+
+using Meter_d = Unit<Meter, Length>;
+
 
 
 }
--- tests/UnitTest.cpp
+++ tests/UnitTest.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ratio>
 #include "gtest/gtest.h"
 #include "hummingbird/solve/Unit.h"
 
@@ -26,4 +27,59 @@ TEST(UnitTest, Start)
 
 	Unit<float, MulType<Dimension<1,0,0>, Dimension<0,1,0>>> u1(2);
 	Unit<float, MulType<Mass, Length>> u2(2);
+}
+
+TEST(UnitTest, Units)
+{
+	ratio<1,10> r;
+
+	ratio<1,10>::type r1;
+
+	cout << decltype(r1)::num << "/" << r1.den << endl;
+
+	Quantity<float, ratio<1,100>> width(6);
+	Centimeter height(2);
+	Meter length(5);
+
+	decltype(height)::unit u;
+
+	cout << decltype(u)::num << "/" << u.den << endl;
+
+	cout << height * length;
+
+	ratio_add<ratio<2,5>,ratio<1,3>> r2;
+	cout << decltype(r2)::num << "/" << r2.den << endl;
+
+	Quantity<float, ratio<2,5>> a(4);
+	Quantity<float, ratio<1,3>> b(2.5);
+	cout << "a + b = " << a + b << endl;
+
+	BaseUnit<ratio<2,5>, ratio<1,3>>::type r3;
+	cout << "this: " << decltype(r3)::num << "/" << r3.den << endl;
+
+	cout << height + length << endl;
+
+	cout << 20 % 40 << " and " << 41 % 20 << endl;
+
+	cout << "IsMultiple " << IsMultiple<ratio<1,3>, ratio<1,6>>::value << endl;
+	//cout << "IsMultipleTest " << IsMultipleTest<ratio<1,23>, ratio<1,22>>::value << endl;
+
+	cout << "std::is_integral<T>::value " << std::is_integral<int>::value << endl;
+
+
+}
+
+TEST(UnitTest, ImplicitConversion)
+{
+	Quantity<int, ratio<1,1>> a1(1);
+	Quantity<int, ratio<1,3>> a2(1);
+	//Quantity<int, ratio<1,1>> a3fail(a2);
+	Quantity<int, ratio<1,6>> a3(a2);
+
+	Quantity<int, ratio<1,3>> a4(a1);
+	cout << "a4 " << a4 << endl;
+	a3 += a4;
+	cout << "a3 " << a4 << endl;
+
+
 }
